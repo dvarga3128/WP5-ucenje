c913dbd9d9fb23b8fc7f376a088ac7a427a5f881
Danijela Varga <danijelavarga.mail@gmail.com>
2024-06-06 18:02:37 +0200

    ucenje dizajn početak
    kod za tablicu

---
+++ ucenjedizajn.sql
@@ -0,0 +1,28 @@
+--create database ucenjedizajn;
+
+--create table klijenti(
+--sifra int,
+--naziv varchar(100),
+--projekt varchar(100),
+--email varchar(100)
+--);
+
+--create table projekti(
+--sifra int,
+--naziv varchar(100),
+--usluga varchar(100),
+--dizajner varchar(100)
+--);
+
+--create table usluge(
+--sifra int,
+--naziv varchar(100),
+--cijena int
+--);
+
+--create table dizajner(
+--sifra int,
+--ime varchar(20),
+--prezime varchar(20),
+--email varchar(100)
+--);
