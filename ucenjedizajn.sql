3646090b88ea807264b162a9cf4207072633c158
Danijela Varga <danijelavarga.mail@gmail.com>
2024-06-11 14:27:45 +0200

    Update ucenjedizajn.sql

--- ucenjedizajn.sql
+++ ucenjedizajn.sql
@@ -44,15 +44,21 @@ insert into klijenti (naziv,email) values
 ('Pero Perke', 'pperic@gmail.com'),
 ('Josjednafirma','firma22@gmail.com');
 
+--select * from usluge
+
 insert into usluge (naziv,cijena) values
 ('vizualni identitet', 500.50),
 ('digitalni marketing', 700.99),
 ('logotip',370.00);
 
+--select * from dizajneri
+
 insert into dizajneri (ime,prezime,email) values
 ('ana','marek','anamarek@gmail.com'),
 ('ime', 'prezime', 'ime.prezime1@hotmail.com'),
 ('dobar','dizajner', 'dragon-rider2@gmail.com');
+
+--select * from projekti
 
 insert into projekti (naziv,usluga,dizajner,klijent) values
 ('rebranding',1,3,2),('novi logo',3,1,1),('sm marketing',2,2,3);
