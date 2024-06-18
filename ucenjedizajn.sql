de87c4599c26d627c54039977dbfe9ea280d1fa8
Danijela Varga <danijelavarga.mail@gmail.com>
2024-06-18 16:37:13 +0200

    Update ucenjedizajn.sql
    Napravljen insert

--- ucenjedizajn.sql
+++ ucenjedizajn.sql
@@ -1,10 +1,11 @@
-use master
+use master;
 go
-drop database if exists ucenjedizajn
-go 
-create database ucenjedizajn
+drop database if exists ucenjedizajn;
 go
-use ucenjedizajn
+create database ucenjedizajn;
+go
+
+use ucenjedizajn;
 
 create table klijenti(
 sifra int not null primary key identity(1,1),
@@ -37,20 +38,28 @@ alter table projekti add foreign key (klijent) references klijenti(sifra);
 alter table projekti add foreign key (usluga) references usluge(sifra);
 alter table projekti add foreign key(dizajner) references dizajneri(sifra);
 
+select * from klijenti;
+
 insert into klijenti (naziv,email) values
 ('LegitFirma','legitfirma@gmail.com'),
 ('Pero Perke', 'pperic@gmail.com'),
 ('Josjednafirma','firma22@gmail.com');
+
+select * from usluge;
 
 insert into usluge (naziv,cijena) values
 ('vizualni identitet', 500.50),
 ('digitalni marketing', 700.99),
 ('logotip',370.00);
 
+select * from dizajneri;
+
 insert into dizajneri (ime,prezime,email) values
 ('ana','marek','anamarek@gmail.com'),
 ('ime', 'prezime', 'ime.prezime1@hotmail.com'),
 ('dobar','dizajner', 'dragon-rider2@gmail.com');
+
+select * from projekti;
 
 insert into projekti (naziv,usluga,dizajner,klijent) values
 ('rebranding',1,3,2),('novi logo',3,1,1),('sm marketing',2,2,3);
