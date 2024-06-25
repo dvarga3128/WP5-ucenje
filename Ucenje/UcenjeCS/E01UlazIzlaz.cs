fd9643826724419f91f26ad1da863431eaab91c8
Danijela Varga <danijelavarga.mail@gmail.com>
2024-06-25 21:06:48 +0200

    csharp

---
+++ Ucenje/UcenjeCS/E01UlazIzlaz.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E01UlazIzlaz
+    {
+
+        public static void Izvedi()
+        {
+
+            Console.WriteLine("Hello");
+
+            Console.Write("Upiši svoje ime: ");
+
+            string ime = Console.ReadLine();
+
+            Console.WriteLine("Unijeli ste " + ime);
+            //formatirani način zapisa
+            Console.WriteLine("Unijeli ste {0}", ime);
+
+
+            //adresa i grad
+
+            Console.Write("Unesi adresu: ");
+
+            string adresa = Console.ReadLine();
+
+            Console.Write("Unesi grad: ");
+
+            string grad = Console.ReadLine();
+
+
+            Console.WriteLine("Unijeli ste " + adresa);
+            Console.WriteLine("Unijeli ste " + grad);
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Ucenje/UcenjeCS/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+using UcenjeCS;
+
+E02VarijableTipoviPodataka.Izvedi();
+
+
+//E01UlazIzlaz.Izvedi();
+
+
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
